4a3224d0a8c0436064a85df8c714876f9f75dad5
Ella <ellawu010@gmail.com>
2025-07-12 21:53:32 +0800

    wip

--- src/layout/LayoutComm.tsx
+++ src/layout/LayoutComm.tsx
@@ -9,7 +9,6 @@ const LayoutComm = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            {/* <div className="bg-bookingblue py-3"></div> */}
             <div className="container mx-auto py-10 flex-1">{children}</div>
             <Footer />
         </div>
